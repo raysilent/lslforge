528ac7021ff6ab5258239af138a9f8b1f736f9b4
Ray Silent <ray.silent@gmail.com>
2018-02-04 22:35:50 -0500

    Explain stack configuration. Fix paths to LSLForge binary

--- lslforge/copy_win32.bat
+++ lslforge/copy_win32.bat
@@ -3,8 +3,8 @@
 whoami /groups | find "S-1-16-12288" > nul
 
 if %errorlevel% == 0 (
-  stack exec strip C:\Users\User\AppData\Roaming\local\bin\LslForge.exe
-  copy C:\Users\User\AppData\Roaming\local\bin\LslForge.exe eclipse\lslforge-win32-x86\os\win32\x86
+  stack exec strip haskell\.stack-work\dist\ca59d0ab\build\LslForge\LslForge.exe
+  copy haskell\.stack-work\dist\ca59d0ab\build\LslForge\LslForge.exe eclipse\lslforge-win32-x86\os\win32\x86
   timeout /t 60
 ) else (
   echo. 
