04ebd23136ef30e636261d72b38fdb44a03bfe5d
Ray Silent <ray.silent@gmail.com>
2018-09-18 10:52:23 -0400

    Silent windows copy

--- lslforge/copy_win32.bat
+++ lslforge/copy_win32.bat
@@ -1,5 +1,5 @@
 @echo off
 
 rem stack exec strip haskell\.stack-work\dist\ca59d0ab\build\LslForge\LSLForge.exe
-xcopy /s haskell\.stack-work\dist\ca59d0ab\build\LslForge\LSLForge.exe eclipse\lslforge-win32-x86\os\win32\x86
+xcopy /y haskell\.stack-work\dist\ca59d0ab\build\LslForge\LSLForge.exe eclipse\lslforge-win32-x86\os\win32\x86
 rem timeout /t 60
